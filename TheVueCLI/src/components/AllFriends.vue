bd6248c2616fae41e0315bb5c8a91a1efeacf816
thetarnav <gthetarnav@gmail.com>
2020-01-06 00:57:06 +0100

    passing data by props

---
+++ TheVueCLI/src/components/AllFriends.vue
@@ -0,0 +1,21 @@
+<template>
+   <div id="allFriends">
+      <h2>All Frends</h2>
+      <div v-for="(friend, index) in friends" :key="index">
+         <i>{{ index }}</i>
+         <span>{{ friend.name }}</span>
+      </div>
+   </div>
+</template>
+
+<script>
+export default {
+   name: "AllFriends",
+   props: ["friends"],
+   data() {
+      return {};
+   }
+};
+</script>
+
+<style lang="sass" scoped></style>
---
+++ TheVueCLI/src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav>
+	  <ul>
+		  <li><a href="">Home</a></li>
+		  <li><a href="">About</a></li>
+		  <li><a href="">Contact</a></li>
+	  </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+	name: 'Navbar',
+	data() {
+		return {
+			
+		}
+	},
+}
+</script>
+
+<style lang="sass" scoped>
+nav
+	text-align: center
+
+ul
+	padding: 0
+
+li
+	display: inline-block
+	list-style-type: none
+	margin: 10px
+</style>
