b9d4dee8a5cb0418d00dffe1c27ed983c6100b2d
thetarnav <gthetarnav@gmail.com>
2020-01-19 23:53:23 +0100

    Created basic views, welcome form, router with props passing and route guard

--- ninja-chat/src/router/index.js
+++ ninja-chat/src/router/index.js
@@ -1,27 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Welcome from '../views/Welcome.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+	{
+		path: '/',
+		name: 'Welcome',
+		component: Welcome,
+	},
+	{
+		path: '/chat',
+		name: 'Chat',
+		component: () => import('../views/Chat.vue'),
+		props: true,
+		beforeEnter: (to, from, next) => {
+			if (to.params.name) next()
+			else next({ name: 'Welcome' })
+		}
+	},
 ]
 
 const router = new VueRouter({
-  routes
+	routes,
 })
 
 export default router
---
+++ ninja-chat/src/views/Chat.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="chat container">
+		<h2>Chat</h2>
+		<p>I'm {{name}}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Chat',
+	props: ['name'],
+	data() {
+		return {}
+	},
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
