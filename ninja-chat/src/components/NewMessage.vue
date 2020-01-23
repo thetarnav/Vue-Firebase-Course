9f2ac4244e1d6f622ff050e6dd221477c41a89e6
thetarnav <gthetarnav@gmail.com>
2020-01-23 20:30:17 +0100

    Real time chat is now working

---
+++ ninja-chat/src/components/NewMessage.vue
@@ -0,0 +1,65 @@
+<template>
+	<div class="new-message">
+		<form @submit.prevent="addMessage">
+			<div class="field">
+				<label for="new-message">New Message</label>
+				<input type="text" name="new-message" v-model="newMessage" />
+			</div>
+			<button class="btn-small teal">
+				<i class="material-icons">send</i>
+			</button>
+		</form>
+		<Feedback :message="feedback" v-if="feedback" />
+	</div>
+</template>
+
+<script>
+import db from '../firebase/init'
+
+export default {
+	name: 'NewMessage',
+	components: {
+		Feedback: () => import('./Feedback'),
+	},
+	props: ['name'],
+	data() {
+		return {
+			newMessage: null,
+			feedback: null,
+		}
+	},
+	methods: {
+		addMessage() {
+			if (!this.newMessage)
+				this.feedback = 'You must enter something in order to send it'
+			else {
+				db.collection('messages')
+					.add({
+						content: this.newMessage,
+						name: this.name,
+						timestamp: Date.now(),
+					})
+					.catch(console.error)
+				this.newMessage = null
+				this.feedback = null
+			}
+		},
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+form {
+	display: flex;
+	justify-content: stretch;
+	align-items: flex-end;
+	margin-bottom: 5px;
+}
+.field {
+	flex-grow: 1;
+	margin-right: 20px;
+}
+button {
+	margin-bottom: 5px;
+}
+</style>
--- ninja-chat/src/views/Chat.vue
+++ ninja-chat/src/views/Chat.vue
@@ -1,20 +1,74 @@
 <template>
 	<div class="chat container">
-		<h2>Chat</h2>
-		<p>I'm {{name}}</p>
+		<h2 class="center teal-text">Real-time chat</h2>
+		<div class="card">
+			<div class="card-content">
+				<ul class="messages">
+					<li v-for="{id, name, content, timestamp} in messages" :key="id">
+						<span class="teal-text">{{name}}</span>
+						<span class="grey-text text-darken-3 message">{{content}}</span>
+						<span class="grey-text time">{{timestamp}}</span>
+					</li>
+				</ul>
+			</div>
+			<div class="card-action">
+				<NewMessage :name="name" />
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
+import NewMessage from '../components/NewMessage'
+import db from '../firebase/init'
+
 export default {
 	name: 'Chat',
+	components: {
+		NewMessage,
+	},
 	props: ['name'],
 	data() {
-		return {}
+		return {
+			messages: [],
+		}
+	},
+	created() {
+		const ref = db.collection('messages').orderBy('timestamp')
+		ref.onSnapshot(snapshot => {
+			const changes = snapshot.docChanges()
+			changes.forEach(change => {
+				if (change.type !== 'added') return
+
+				const doc = change.doc,
+					id = doc.id,
+					{ name, content, timestamp } = doc.data()
+				this.messages.push({
+					id,
+					name,
+					content,
+					timestamp,
+				})
+			})
+		})
 	},
 }
 </script>
 
 <style lang="sass" scoped>
+.container
+	max-width: 400px
+h2
+	font-size: 2.6em
+	margin-bottom: 40px
 
+span
+	font-size: 1.4em
+
+.message
+	margin-left: 10px
+
+.time
+	display: block
+	font-size: 1.2em
 </style>
