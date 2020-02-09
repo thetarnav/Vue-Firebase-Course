34ee866b0d84591b08f929ccf9d580b7fc0bf437
Damian Tarnawski @thetarnav <gthetarnav@gmail.com>
2020-02-09 17:05:44 +0100

    Used firebase functions for checking alias uniqueness in user signup, instead of checking that on the frontend.

--- geo-ninjas/functions/index.js
+++ geo-ninjas/functions/index.js
@@ -1,8 +1,22 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// initializing accessibility to firestore database:
+const admin = require('firebase-admin')
+admin.initializeApp()
+
+// function responsible for checking alias uniqueness
+exports.checkAlias = functions.https.onCall((data, context) => {
+	const ref = admin
+		.firestore()
+		.collection('users')
+		.doc(data.slug)
+
+	return ref
+		.get()
+		.then(doc => ({
+			unique: !doc.exists,
+		}))
+		.catch(() => {
+			throw new functions.https.HttpsError('failed to connect')
+		})
+})
